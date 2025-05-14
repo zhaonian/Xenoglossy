7da3aa9f3189e2dc7a5b3747149416d68183710b
Zhaonian Luan <lzn0830@gmail.com>
2025-05-13 17:38:00 -0700

    Add menu icon

--- Xenoglossy/XenoglossyApp.swift
+++ Xenoglossy/XenoglossyApp.swift
@@ -9,9 +9,38 @@ import SwiftUI
 
 @main
 struct XenoglossyApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var statusItem: NSStatusItem!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Create the status bar item
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        
+        // Set the icon and behavior
+        if let button = statusItem.button {
+            button.image = NSImage(systemSymbolName: "gear", accessibilityDescription: "Xenoglossy")
+        }
+        
+        // Create a menu for the status bar item
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Settings", action: #selector(quitApp), keyEquivalent: "s"))
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q"))
+        statusItem.menu = menu
+    }
+    
+    @objc func quitApp() {
+        NSApplication.shared.terminate(self)
     }
 }
