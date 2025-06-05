5cfa7ef06614426ff07d820670274535f9d8a85e
Zhao Luan <zluan@adobe.com>
2025-06-05 16:08:06 -0700

    Make a release

--- Xenoglossy/Config.template.swift
+++ Xenoglossy/Config.template.swift
@@ -2,7 +2,7 @@ import Foundation
 
 // This is an example configuration file.
 // Copy this file to Config.swift and replace the placeholder values with your API keys.
-//enum Config {
-//    static let openAIKey = "YOUR_OPENAI_API_KEY"
-//    static let geminiKey = "YOUR_GEMINI_API_KEY"
-//} 
+enum Config {
+    static let openAIKey = "YOUR_OPENAI_API_KEY"
+    static let geminiKey = "YOUR_GEMINI_API_KEY"
+} 
